7fc82eca3472a6885edb95449df7f9813d7bca80
Matt Smith <96mattsmith@gmail.com>
2019-02-05 09:30:30 -0700

    updated then function

--- actions/then/servicenow.go
+++ actions/then/servicenow.go
@@ -33,48 +33,29 @@ func CreateIncident(ctx context.Context, with []byte) *nerr.E {
 	}
 
 	//pass alert and call the create incident function is ServiceNow
-	incident structs., err := servicenow.CreateIncident(alert)
-	if err != nil {
-		log.L.Errorf("Failed to create incident")
-		return "", nerr.Translate(err).Add("Incident was not created in servicenow")
+	if alert.IncidentID == nil {
+		incident, err := servicenow.CreateIncident(alert)
+		if err != nil {
+			log.L.Errorf("Failed to create incident")
+			return "", nerr.Translate(err).Add("Incident was not created in servicenow")
+		}
+		alert.IncidentID = incident.Number
+		AddAlert(alert)
 	}
-	alert.IncidentID=incident.Number
-	AddAlert(alert)
-
-	return nil
-}
-
-func ModifyIncident(ctx context.Context, with []byte) *nerr.E {
-
-	alert, ok := alertctx.GetAlert(ctx)
-	if !ok {
-		log.L.Errorf("Failed to get Alert")
-		return "", nerr.Create("Must have alert to modify incident", "")
+	if alert.IncidentID != nil && alert.Resolved == false {
+		incident, err := servicenow.ModifyIncident(alert)
+		if err != nil {
+			log.L.Errorf("Failed to Modify incident")
+			return "", nerr.Translate(err).Add("Incident was not modified in servicenow")
+		}
 	}
 
-	//pass alert and call the create incident function is ServiceNow
-	incident, err := servicenow.ModifyIncident(alert)
-	if err != nil {
-		log.L.Errorf("Failed to Modify incident")
-		return "", nerr.Translate(err).Add("Incident was not modified in servicenow")
+	if alert.IncidentID != nil && alert.Resolved == true {
+		incident, err := servicenow.CloseIncident(alert)
+		if err != nil {
+			log.L.Errorf("Failed to close incident")
+			return "", nerr.Translate(err).Add("Incident was not closed in servicenow")
+		}
 	}
-
-	return nil
-}
-
-func CloseIncident(ctx context.Context, with []byte) *nerr.E {
-	alert, ok := alertctx.GetAlert(ctx)
-	if !ok {
-		log.L.Errorf("Failed to get Alert")
-		return "", nerr.Create("Must have alert to close incident", "")
-	}
-
-	//pass alert and call the create incident function is ServiceNow
-	incident, err := servicenow.CloseIncident(alert)
-	if err != nil {
-		log.L.Errorf("Failed to close incident")
-		return "", nerr.Translate(err).Add("Incident was not closed in servicenow")
-	}
-
 	return nil
 }
