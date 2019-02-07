06b8649bc582eb3393e0773f3f0564d0e25c1b45
Matt Smith <96mattsmith@gmail.com>
2019-02-07 08:16:35 -0700

    commit

--- actions/then/servicenow.go
+++ actions/then/servicenow.go
@@ -10,17 +10,11 @@ import (
 	"github.com/byuoitav/shipwright/alertstore"
 )
 
-//would this need to return the incident? or what are we doing with it/where are we
-//storing incident info?
-
-//TODO: create only one then statement (if there is no incident number Then create incident)
-//If there is an incident number and it is not resolved yet, then modify incident
-//if there is an incident number AND it is resolved, then close the incident.
-func CreateIncident(ctx context.Context, with []byte) *nerr.E {
+func CreateIncidentRepair(ctx context.Context, with []byte) (id string *nerr.E) {
 	alert, ok := actionctx.GetAlert(ctx)
 	if !ok {
 		log.L.Errorf("Failed to get Alert")
-		return nerr.Create("Must have alert to create incident", "")
+		return "",nerr.Create("Must have alert to create incident", "")
 	}
 
 	//pass alert and call the create incident function is ServiceNow
@@ -29,33 +23,39 @@ func CreateIncident(ctx context.Context, with []byte) *nerr.E {
 			incident, err := servicenow.CreateIncident(alert)
 			if err != nil {
 				log.L.Errorf("Failed to create incident")
-				return nerr.Translate(err).Add("Incident was not created in servicenow")
+				return "",nerr.Translate(err).Add("Incident was not created in servicenow")
 			}
 			alert.IncidentID = incident.Number
+			alertstore.AddAlert(alert)
+			return incident.Number, nil
 		} else {
 			repair, err := servicenow.CreateRepair(alert)
 			if err != nil {
 				log.L.Errorf("Failed to create repair")
-				return nerr.Translate(err).Add("repair was not created in servicenow")
+				return "",nerr.Translate(err).Add("repair was not created in servicenow")
 			}
 			alert.IncidentID = repair.Number
+			alertstore.AddAlert(alert)
+			return repair.Number, nil
 		}
 
-		alertstore.AddAlert(alert)
+
 	}
 	if len(alert.IncidentID) != 0 && alert.Resolved == false {
 		if alert.Severity == "critical" {
 			incident, err := servicenow.ModifyIncident(alert)
 			if err != nil {
 				log.L.Errorf("Failed to Modify incident")
-				return nerr.Translate(err).Add("Incident was not modified in servicenow")
+				return "",nerr.Translate(err).Add("Incident was not modified in servicenow")
 			}
+			return incident.Number, nil
 		} else {
 			repair, err := servicenow.ModifyRepair(alert)
 			if err != nil {
 				log.L.Errorf("Failed to Modify repair")
-				return nerr.Translate(err).Add("repair was not modified in servicenow")
+				return "",nerr.Translate(err).Add("repair was not modified in servicenow")
 			}
+			return repair.Number, nil
 		}
 
 	}
@@ -65,14 +65,16 @@ func CreateIncident(ctx context.Context, with []byte) *nerr.E {
 			incident, err := servicenow.CloseIncident(alert)
 			if err != nil {
 				log.L.Errorf("Failed to close incident")
-				return nerr.Translate(err).Add("Incident was not closed in servicenow")
+				return "",nerr.Translate(err).Add("Incident was not closed in servicenow")
 			}
+			return incident.Number, nil
 		} else {
 			repair, err := servicenow.CloseRepair(alert)
 			if err != nil {
 				log.L.Errorf("Failed to close repair")
-				return nerr.Translate(err).Add("Repair was not closed in servicenow")
+				return "",nerr.Translate(err).Add("Repair was not closed in servicenow")
 			}
+			return repair.Number, nil
 		}
 
 	}
