3896772f8c2fc628d1c4a196345b4d369d61b663
jpw547 <jwerner547@gmail.com>
2019-02-13 11:02:22 -0700

    this is all that I think won't break things...

--- web/src/app/modals/respond/respond.component.ts
+++ web/src/app/modals/respond/respond.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { StringsService } from 'src/app/services/strings.service';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { RoomAlerts } from 'src/app/objects';
 
 @Component({
   selector: 'respond-modal',
@@ -7,8 +9,19 @@ import { StringsService } from 'src/app/services/strings.service';
   styleUrls: ['./respond.component.scss']
 })
 export class RespondModalComponent implements OnInit {
+  sentHelpDate: Date = new Date();
+  helpArrivedDate: Date = new Date();
+  ra: RoomAlerts = new RoomAlerts();
 
-  constructor(public text: StringsService) {
+  sentTime: string;
+  arrivedTime: string;
+
+  constructor(public text: StringsService, public dialogRef: MatDialogRef<RespondModalComponent>, @Inject(MAT_DIALOG_DATA) data: RoomAlerts) {
+    if(data != null) {
+      Object.assign(this.ra, data)
+      console.log(this.ra.SentIsZero())
+      console.log(this.ra.ArriveIsZero())
+    }
   }
 
   ngOnInit() {
