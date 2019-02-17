05596ef7323c205d4dbf242fb011ba304a924ffa
Joseph Blodgett <joseph.blodgett@gmail.com>
2019-02-16 17:54:36 -0700

    resub to *

--- server.go
+++ server.go
@@ -49,7 +49,7 @@ func main() {
 
 	// get events from the hub
 	go func() {
-		messenger.SubscribeToRooms("ITB-2019")
+		messenger.SubscribeToRooms("*")
 
 		for {
 			processEvent(messenger.ReceiveEvent())
