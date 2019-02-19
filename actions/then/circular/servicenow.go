ed16689641176b9d97bff0a4d7bcd8f2e8d94ca1
thailandmatt <31248374+thailandmatt@users.noreply.github.com>
2019-02-18 19:07:49 -0700

    updated then for servicenow

--- actions/then/circular/servicenow.go
+++ actions/then/circular/servicenow.go
@@ -7,80 +7,26 @@ import (
 	"github.com/byuoitav/common/nerr"
 	"github.com/byuoitav/common/servicenow"
 	"github.com/byuoitav/shipwright/actions/actionctx"
-	"github.com/byuoitav/shipwright/alertstore"
 )
 
-func CreateIncidentRepair(ctx context.Context, with []byte) (id string, err *nerr.E) {
-	alert, ok := actionctx.GetAlert(ctx)
+//SyncRoomIssueWithServiceNow will sync the RoomIssue with an ticket (incident or repair)
+func SyncRoomIssueWithServiceNow(ctx context.Context, with []byte) (err *nerr.E) {
+	//get the RoomIssue from the context
+	roomIssue, ok := actionctx.GetRoomIssue(ctx)
+
 	if !ok {
-		log.L.Errorf("Failed to get Alert")
-		return "", nerr.Create("Must have alert to create incident", "")
+		log.L.Errorf("Failed to get RoomIssue")
+		return nerr.Create("Must have RoomIssue to create ticket", "")
 	}
 
-	//pass alert and call the create incident function is ServiceNow
-	if len(alert.IncidentID) == 0 {
-		if alert.Severity == "critical" {
-			incident, err := servicenow.CreateIncident(alert)
-			if err != nil {
-				log.L.Errorf("Failed to create incident")
-				return "", nerr.Translate(err).Add("Incident was not created in servicenow")
-			}
-			alert.IncidentID = incident.Number
-			alertstore.AddAlert(alert)
-			log.L.Infof("ticket number: %s", incident.Number)
-			return incident.Number, nil
-		} else {
-			repair, err := servicenow.CreateRepair(alert)
-			if err != nil {
-				log.L.Errorf("Failed to create repair")
-				return "", nerr.Translate(err).Add("repair was not created in servicenow")
-			}
-			alert.IncidentID = repair.Number
-			alertstore.AddAlert(alert)
-			log.L.Infof("ticket number: %s", repair.Number)
-			return repair.Number, nil
-		}
+	incidentID, err := servicenow.SyncServiceNowWithRoomIssue(roomIssue)
 
+	if err != nil {
+		log.L.Errorf("Unable to sync incident")
 	}
 
-	if len(alert.IncidentID) != 0 && alert.Resolved == false {
-		if alert.Severity == "critical" {
-			incident, err := servicenow.ModifyIncident(alert)
-			if err != nil {
-				log.L.Errorf("Failed to Modify incident")
-				return "", nerr.Translate(err).Add("Incident was not modified in servicenow")
-			}
-			log.L.Infof("ticket number: %s", incident.Number)
-			return incident.Number, nil
-		} else {
-			repair, err := servicenow.ModifyRepair(alert)
-			if err != nil {
-				log.L.Errorf("Failed to Modify repair")
-				return "", nerr.Translate(err).Add("repair was not modified in servicenow")
-			}
-			log.L.Infof("ticket number: %s", repair.Number)
-			return repair.Number, nil
-		}
+	if len(roomIssue.IncidentID) == 0 {
+		roomIssue.IncidentID = incidentID
 	}
 
-	if len(alert.IncidentID) != 0 && alert.Resolved == true {
-		if alert.Severity == "critical" {
-			incident, err := servicenow.CloseIncident(alert)
-			if err != nil {
-				log.L.Errorf("Failed to close incident")
-				return "", nerr.Translate(err).Add("Incident was not closed in servicenow")
-			}
-			log.L.Infof("ticket number: %s", incident.Number)
-			return incident.Number, nil
-		} else {
-			repair, err := servicenow.CloseRepair(alert)
-			if err != nil {
-				log.L.Errorf("Failed to close repair")
-				return "", nerr.Translate(err).Add("Repair was not closed in servicenow")
-			}
-			log.L.Infof("ticket number: %s", repair.Number)
-			return repair.Number, nil
-		}
-	}
-	return "", nil
 }
