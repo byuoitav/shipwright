4b0b19ac9046cf2ea88786fff93682626460a270
Matt Blodgett <mblodget@chiangmai.byu.edu>
2019-02-28 13:35:33 -0700

    fix bug

--- web/src/app/components/dashboard/batteryreport/batteryreport.component.ts
+++ web/src/app/components/dashboard/batteryreport/batteryreport.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { StringsService } from 'src/app/services/strings.service';
 import { IDashPanel } from '../dashpanel/idashpanel';
+import { DashPanelTypes } from 'src/app/services/dashpanel.service';
 
 @Component({
   selector: 'battery-report',
@@ -9,7 +10,7 @@ import { IDashPanel } from '../dashpanel/idashpanel';
 })
 export class BatteryReportComponent implements OnInit, IDashPanel {
   @Input() info: any;
-  @Input() chosenSeverity: string;
+  @Input() chosenSeverity: DashPanelTypes;
   
   constructor(public text: StringsService) { }
 
