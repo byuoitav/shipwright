112acab4a18b877a4eb2d4b5d955c560c8963c79
jpw547 <jwerner547@gmail.com>
2019-03-08 14:34:16 -0700

    prepping for the merge

--- web/src/app/services/strings.service.ts
+++ web/src/app/services/strings.service.ts
@@ -20,31 +20,31 @@ export class StringsService {
 
   DefaultIcons = {
     "ADCP Sony VPL": "videocam",
-    AppleTV: "airplay",
-    Aruba8PortNetworkSwitch: "settings_ethernet",
-    Blu50: "speaker",
-    ChromeCast: "cast",
-    Computer: "desktop_windows",
+    "AppleTV": "airplay",
+    "Aruba8PortNetworkSwitch": "settings_ethernet",
+    "Blu50": "speaker",
+    "ChromeCast": "cast",
+    "Computer": "desktop_windows",
     "Crestron RMC-3 Gateway": "security",
     "DM-MD16x16": "device_hub",
-    DividerSensors: "leak_add",
+    "DividerSensors": "leak_add",
     "DMPS": "accessible_forward",
-    FunnelGateway: "security",
-    Gefen4x1: "device_hub",
-    JAP3GRX: "flip_to_back",
-    JAP3GTX: "flip_to_front",
+    "FunnelGateway": "security",
+    "Gefen4x1": "device_hub",
+    "JAP3GRX": "flip_to_back",
+    "JAP3GTX": "flip_to_front",
     "Kramer VS-44DT": "device_hub",
     "NEC P502HL": "videocam",
-    Pi3: "video_label",
-    PulseEight8x8: "device_hub",
+    "Pi3": "video_label",
+    "PulseEight8x8": "device_hub",
     "QSC-Core-110F": "surround_sound",
-    SchedulingPanel: "calendar_today",
+    "SchedulingPanel": "calendar_today",
     "Shure Microphone": "mic",
-    ShureULXD: "router",
-    SonyPHZ10: "videocam",
-    SonyVPL: "videocam",
-    SonyXBR: "tv",
-    VideoCard: "add_to_queue",
+    "ShureULXD": "router",
+    "SonyPHZ10": "videocam",
+    "SonyVPL": "videocam",
+    "SonyXBR": "tv",
+    "VideoCard": "add_to_queue",
     "non-controllable": "settings_input_hdmi",
     "via-connect-pro": "settings_input_antenna",
     "control-processor":"video_label",
@@ -72,7 +72,7 @@ export class StringsService {
   }
 
   public RemoveTag(tag: string, data: any): void {
-    let index = data.tags.indexOf(tag);
+    const index = data.tags.indexOf(tag);
     if (index >= 0) {
       data.tags.splice(index, 1);
     }
