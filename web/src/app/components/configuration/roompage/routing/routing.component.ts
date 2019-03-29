9a1f80de82c81859241b3ea895b830edfd3c5089
Daniel Randall <dgrandall@me.com>
2019-03-29 12:09:31 -0600

    get sources

--- web/src/app/components/configuration/roompage/routing/routing.component.ts
+++ web/src/app/components/configuration/roompage/routing/routing.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+
+import { Room } from "src/app/objects/database";
+import { APIService } from "src/app/services/api.service";
+
+enum RoutingType {
+  VIDEO
+}
 
 @Component({
-  selector: 'routing',
-  templateUrl: './routing.component.html',
-  styleUrls: ['./routing.component.scss']
+  selector: "routing",
+  templateUrl: "./routing.component.html",
+  styleUrls: ["./routing.component.scss"]
 })
 export class RoutingComponent implements OnInit {
+  @Input() roomID: string;
+  room: Room;
 
-  constructor() { }
+  constructor(private api: APIService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
+    this.room = await this.api.GetRoom(this.roomID);
+    console.log("routing room", this.room);
+
+    console.log("sources", this.sources(RoutingType.VIDEO));
   }
 
+  sources(routingType: RoutingType): string[] {
+    let map: Map<string, string[]>;
+    switch (routingType) {
+      case RoutingType.VIDEO:
+        map = this.room.signalPaths.video;
+        break;
+      default:
+        map = undefined;
+    }
+
+    if (!map) {
+      return undefined;
+    }
+
+    const sources = [];
+    for (const val of map.values()) {
+      val
+        .filter(v => !map.has(v))
+        .forEach(v => {
+          sources.push(v);
+        });
+    }
+
+    return sources;
+  }
+
+  outputs(routingType: string): string[] {
+    return [];
+  }
 }
