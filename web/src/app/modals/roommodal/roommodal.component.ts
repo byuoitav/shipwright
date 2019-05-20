e9e6759ee4e4bca70c131375726f5ce4f8418c9a
jpw547 <jwerner547@gmail.com>
2019-05-20 11:13:17 -0600

    fixing some room things

--- web/src/app/modals/roommodal/roommodal.component.ts
+++ web/src/app/modals/roommodal/roommodal.component.ts
@@ -3,7 +3,7 @@ import { StringsService } from "src/app/services/strings.service";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 import { APIService} from "src/app/services/api.service";
 import { DataService} from "src/app/services/data.service";
-import { Room } from "src/app/objects/database";
+import { Room, DBResponse } from "src/app/objects/database";
 
 @Component({
   selector: "room-modal",
@@ -19,22 +19,80 @@ export class RoomModalComponent implements OnInit {
   ngOnInit() {
   }
 
-  AddRoom() {
-    if (this.data.id == null || this.data.name == null || this.data.configuration == null || this.data.designation == null) {
-      return;
+  // AddRoom() {
+  //   if (this.data.id == null || this.data.name == null || this.data.configuration == null || this.data.designation == null) {
+  //     return;
+  //   }
+  //   this.api.AddRoom(this.data).then((resp) => {
+  //     this.dialogRef.close(resp);
+  //   });
+  // }
+
+  // UpdateRoom() {
+  //   this.api.UpdateRoom(this.data.id, this.data).then((resp) => {
+  //     this.dialogRef.close(resp);
+  //   });
+  // }
+
+  // CloseModal() {
+  //   this.dialogRef.close();
+  // }
+
+  saveRoom = async (): Promise<boolean> => {
+    this.data.name = this.data.id;
+    console.log("saving room", this.data);
+    try {
+      let resp: DBResponse;
+      if (!this.data.isNew) {
+        resp = await this.api.UpdateRoom(
+          this.data.id,
+          this.data
+        );
+        if (resp.success) {
+          console.log("successfully updated the room", resp);
+        } else {
+          console.error("failed to update the room", resp);
+        }
+      } else {
+        resp = await this.api.AddRoom(this.data);
+
+        if (resp.success) {
+          console.log("successfully added the room", resp);
+          this.data.isNew = false;
+        } else {
+          console.error("failed to add the room", resp);
+        }
+      }
+
+      return resp.success;
+    } catch (e) {
+      console.error("failed to update the room:", e);
+      return false;
     }
-    this.api.AddRoom(this.data).then((resp) => {
-      this.dialogRef.close(resp);
-    });
-  }
+  };
 
-  UpdateRoom() {
-    this.api.UpdateRoom(this.data.id, this.data).then((resp) => {
-      this.dialogRef.close(resp);
-    });
-  }
+  deleteRoom = async (): Promise<boolean> => {
+    console.log("deleting room", this.data);
+    try {
+      if (!this.data.isNew) {
+        let resp: DBResponse;
+        resp = await this.api.DeleteDevice(
+          this.data.id
+        );
+        if (resp.success) {
+          console.log("successfully deleted the room", resp);
+        } else {
+          console.error("failed to delete the room", resp);
+        }
+        return resp.success;
+      }
+    } catch (e) {
+      console.error("failed to delete the room:", e);
+      return false;
+    }
+  };
 
-  CloseModal() {
-    this.dialogRef.close();
+  close(result: any) {
+    this.dialogRef.close(result);
   }
 }
