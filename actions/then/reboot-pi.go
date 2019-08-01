db9a5b5e2ee06216b98a07e01900bda3f75b1908
CalebLarsen <caleb.john.larsen@gmail.com>
2019-08-01 15:54:27 -0600

    Hopefully reboot works lol

--- actions/then/reboot-pi.go
+++ actions/then/reboot-pi.go
@@ -2,9 +2,12 @@ package then
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
+	"github.com/byuoitav/common/db"
 	"github.com/byuoitav/common/log"
 	"github.com/byuoitav/common/nerr"
 	"go.uber.org/zap"
@@ -12,7 +15,7 @@ import (
 
 const (
 	// RebootFrequency .
-	RebootFrequency = 5 * time.Second
+	RebootFrequency = 5 * time.Minute
 )
 
 // RebootInfo .
@@ -58,28 +61,56 @@ func startRebootManager(rebootChan chan RebootInfo) {
 	log := log.L.Named("reboot-manager")
 	log.Infof("Starting reboot manager. Attempting reboots every %v", RebootFrequency)
 
+	rebootMap := make(map[string]bool)
+
 	ticker := time.NewTicker(SlackMessageFrequency)
 	var rebootList []RebootInfo
 
 	for {
 		select {
 		case info := <-rebootChan:
+			log.Infof("Hey at least I'm here: %v", info.DeviceID)
 			rebootList = append(rebootList, info)
 		case <-ticker.C:
-			if rebootList == nil || len(rebootList) == 0 || time.Now().Hour() > 14 {
+			if rebootList == nil || len(rebootList) == 0 {
 				continue
 			}
-			log.Infof("Time added")
-			for _, rebootInfo := range rebootList {
-				log.Infof("Time version rebooting: %s", rebootInfo.DeviceID)
 
-				go reboot(rebootInfo, log)
+			//The hour numbers mean that the pi won't reboot if the hour (in UTC) is less than 6 or greater than 10
+			if time.Now().Hour() < 6 || time.Now().Hour() > 10 {
+				rebootMap = make(map[string]bool)
+				continue
+			}
+
+			for _, rebootInfo := range rebootList {
+				log.Infof("attempting to reboot: %s", rebootInfo.DeviceID)
+				if _, rebooted := rebootMap[rebootInfo.DeviceID]; !rebooted {
+					go reboot(rebootInfo, log)
+				}
+				rebootMap[rebootInfo.DeviceID] = true
 			}
 		}
 	}
 }
 
+//Hit the reboot pi endpoint on the Device Monitoring Microservice
 func reboot(r RebootInfo, log *zap.SugaredLogger) {
-	//SSH in and reboot the pi
-	log.Infof("Rebooting: %v", r.DeviceID)
+	log.Infof("rebooting: %v", r.DeviceID)
+
+	dev, err := db.GetDB().GetDevice(r.DeviceID)
+	if err != nil {
+		log.Warnf("Couldn't get device record: %v", err)
+		return
+	}
+	req, err := http.NewRequest("PUT", fmt.Sprintf("http://%v:10000/device/reboot", dev.Address), nil)
+	if err != nil {
+		log.Warnf("Couldn't make request: %v", err)
+		return
+	}
+	client := &http.Client{}
+	_, err = client.Do(req)
+	if err != nil {
+		log.Warnf("Couldn't reboot pi: %v", err)
+		return
+	}
 }
